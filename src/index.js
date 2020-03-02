333f6967b8d4a87f8184a7baf0005cfaf85b4688
meloneminze <melanie@derrabus.de>
2020-03-02 14:13:20 +0100

    Add basic setup

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function component() {
+  const element = document.createElement('div');
+
+  element.innerHTML = 'Pokedex';
+
+  return element;
+}
+const div = component();
+document.body.appendChild(div);
