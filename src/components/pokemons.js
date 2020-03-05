c8982a9159e4f2c8115e816f080c00e802595668
meloneminze <melanie@derrabus.de>
2020-03-05 12:02:33 +0100

    Limited to favorites

--- src/components/pokemons.js
+++ src/components/pokemons.js
@@ -1,23 +1,33 @@
 import './pokemons.scss';
 import { createElement, appendContent } from '../lib/dom';
 
-export function createSearchResults(items) {
+export function createSearchResults(props) {
   const container = createElement('div', {
     className: 'pokemons'
   });
 
-  items.forEach(item => {
+  props.items.forEach(item => {
     const element = createElement('div', {
       innerText: item,
       className: 'pokemon'
     });
     element.addEventListener('click', () => {
-      const oldFavorites = JSON.parse(localStorage.getItem('favorites')) || [];
+      const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+      // let favorites = JSON.parse(localStorage.getItem('favorites'));
+      // if (!favorites) {
+      //   favorites = [];
+      // }
+
       if (!favorites.includes(item)) {
         favorites.push(item);
       } else {
         const itemIndex = favorites.indexOf(item);
         favorites.splice(itemIndex, 1);
+      }
+
+      if (favorites.length > 3) {
+        // favorites.splice(0, 1);
+        favorites = favorites.slice(1);
       }
 
       const favoritesJSON = JSON.stringify(favorites);
